3e906600e0a1cea131fcf7a6306c1b8dcd0caf26
Cherry <949886@qq.com>
2026-08-16 11:27:02 +0800

    wire player into game root

--- app/GameRoot.cs
+++ app/GameRoot.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Lime.Actors.Player;
 using Lime.World;
 
 namespace Lime.App;
@@ -9,6 +10,7 @@ public partial class GameRoot : Node
     public Node3D WorldRoot { get; private set; } = null!;
     public Node3D LevelRoot { get; private set; } = null!;
     public Node3D ActorRoot { get; private set; } = null!;
+    public PlayerController Player { get; private set; } = null!;
     public Node3D CameraRoot { get; private set; } = null!;
     public CanvasLayer UIRoot { get; private set; } = null!;
     public Node AudioRoot { get; private set; } = null!;
@@ -17,7 +19,18 @@ public partial class GameRoot : Node
     {
         ResolveOwnedNodes();
         ValidateProjectContracts();
+
+        FlowController.StateChanged += SyncGameplayInput;
         FlowController.Start();
+        SyncGameplayInput();
+    }
+
+    public override void _ExitTree()
+    {
+        if (FlowController is not null)
+        {
+            FlowController.StateChanged -= SyncGameplayInput;
+        }
     }
 
     private void ResolveOwnedNodes()
@@ -26,6 +39,7 @@ public partial class GameRoot : Node
         WorldRoot = GetNode<Node3D>("%WorldRoot");
         LevelRoot = GetNode<Node3D>("%LevelRoot");
         ActorRoot = GetNode<Node3D>("%ActorRoot");
+        Player = GetNode<PlayerController>("%Player");
         CameraRoot = GetNode<Node3D>("%CameraRoot");
         UIRoot = GetNode<CanvasLayer>("%UIRoot");
         AudioRoot = GetNode<Node>("%AudioRoot");
@@ -35,5 +49,10 @@ public partial class GameRoot : Node
     {
         InputActions.ValidateConfigured();
         CollisionLayers.ValidateConfigured();
+    }
+
+    private void SyncGameplayInput()
+    {
+        Player.SetInputEnabled(FlowController.GameplayInputEnabled);
     }
 }
