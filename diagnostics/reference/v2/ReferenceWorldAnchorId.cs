59ffcf31d4669a65a51dd80830c0ee64022af58d
Cherry <949886@qq.com>
2026-08-18 13:29:54 +0800

    m1.6.2: add semantic world anchors

---
+++ diagnostics/reference/v2/ReferenceWorldAnchorId.cs
@@ -0,0 +1,23 @@
+namespace Lime.Diagnostics.Reference.V2;
+
+public enum ReferenceWorldAnchorId
+{
+    Unknown = 0,
+    PlayerFeet = 1,
+    StartDeckCenter = 10,
+    StairsATop = 20,
+    StairsABottom = 21,
+    MidTerraceEntry = 30,
+    MidTerraceTurn = 31,
+    StairsBTop = 40,
+    StairsBBottom = 41,
+    TowerPass = 50,
+    LowerCorridorTurn = 60,
+    TreeBend = 70,
+    PoolEntry = 80,
+    PoolCorner = 81,
+    PoolExit = 82,
+    WorkerPlazaEntry = 90,
+    WorkerApproach = 91,
+    FinalExplorePosition = 100,
+}
