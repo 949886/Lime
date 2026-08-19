4f3d263ad8d2d15e529a6c366268e521c20df367
Cherry <949886@qq.com>
2026-08-19 08:05:15 +0800

    M1.6.2: use precise calibration landmark ids

--- diagnostics/reference/v2/ReferenceLandmarkObservation.cs
+++ diagnostics/reference/v2/ReferenceLandmarkObservation.cs
@@ -5,7 +5,7 @@ namespace Lime.Diagnostics.Reference.V2;
 [GlobalClass]
 public partial class ReferenceLandmarkObservation : Resource
 {
-    [Export] public ReferenceWorldAnchorId AnchorId { get; set; }
+    [Export] public ReferenceCalibrationLandmarkId LandmarkId { get; set; }
     [Export] public Vector2 Pixel { get; set; }
     [Export(PropertyHint.Range, "0,1,0.01")] public float Confidence { get; set; } = 1.0f;
     [Export] public bool IsOccluded { get; set; }
